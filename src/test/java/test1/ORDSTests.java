b2b532d1ecd592709e97036a67ac080bc1872129
hackerrank <enginsabanci@gmail.com>
2020-11-08 19:40:29 -0600

    Second Attempt to push.

---
+++ src/test/java/test1/ORDSTests.java
@@ -0,0 +1,13 @@
+package test1;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+public class ORDSTests {
+    //RestAssure is pretty much new class in RestAssured library. We call web service, specify parameters.
+    //We don't have to use class if we import them
+    @Test
+    public void test1 (){
+
+    }
+}
